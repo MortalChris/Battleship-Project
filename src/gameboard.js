95ad033584b5d92c7800c2fb4e1574f3caf2b21e
Christian Valeri <mortlaxris@outlook.com>
2023-06-20 17:10:52 -0500

    visually have a gameBoard

--- src/gameboard.js
+++ src/gameboard.js
@@ -2,14 +2,14 @@ function gameBoardCreation(){
     for(let i = 1; i < 101; i++){
         const player1Board = document.getElementById('player1-board');
             const boardPiece1 = document.createElement("button");
-            boardPiece1.className = "player1-board";
+            boardPiece1.className = "player1-board-piece";
             player1Board.appendChild(boardPiece1);
     }
 
     for(let i = 1; i < 101; i++){
         const player2Board = document.getElementById('player2-board');
             const boardPiece2 = document.createElement("button");
-            boardPiece2.className = "player2-board";
+            boardPiece2.className = "player2-board-piece";
             player2Board.appendChild(boardPiece2);
     }
 };
