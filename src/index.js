742337d8daae47fbfda03eb4c840c71bfc7213aa
Christian Valeri <mortlaxris@outlook.com>
2023-06-28 19:55:20 -0500

    update no clue

--- src/index.js
+++ src/index.js
@@ -42,20 +42,14 @@ function selectShip(){
     const playersObj = players();
     const player1 = playersObj.player1;
     let selectedShip;
-    // const Carrier = document.getElementById("Carrier");
-    // const Battleship = document.getElementById("Battleship");
-    // const Cruiser = document.getElementById("Cruiser");
-    // const Submarine = document.getElementById("Submarine");
-    // const Destroyer = document.getElementById("Destroyer");
+
     const shipList = document.querySelectorAll('.Ship-List');
     shipList.forEach(ship => { // Add a click event listener to each button
         ship.addEventListener('click', () => {
-            // Display the ID of the clicked button
             console.log(ship.id);
             selectedShip = ship.id;
         });
     }); 
-
 
     const currentShip = document.querySelectorAll(".player1-board-piece");
         currentShip.forEach((piece) => {
@@ -72,13 +66,13 @@ selectShip();
 function placeShipsonBoard(board, row, col, direction, count){
         if (direction === 'Horizontal') {
             for (let i = col; i < col + count; i++) {
-                if (i < 10) {
+                if (i < 10 && board[i][row] === 0) {
                 board[row][i] = 1;
                 }
         }
         } else if (direction === 'Vertical') {
             for (let i = row; i < row + count; i++) {
-            if (i < 10) {
+            if (i < 10 && board[i][col] === 0) {
                 board[i][col] = 1;
                 }
             }
